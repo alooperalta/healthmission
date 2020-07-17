fc45a09740b56cd7fb7bfb063984e7c2d7dec2c4
Anuj Talwar <66703039+alooperalta@users.noreply.github.com>
2020-07-17 17:03:17 +0530

    Added the pdf redirect

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 const heading = [
     'Ek Bharat Shrestha Bharat Campaign',
 ];
@@ -26,13 +28,17 @@ const content = [
     'The first EBSB day was observed on 14th February, 2020. The member students gathered near the central library area of the University and invoked Goddess of wisdom. Thereafter, they came to the Flag hosting area and paid homeage to the Pulwama Martyrs. The programme of the day started by taking pledge about the fundamental duties of citizens followed by recital of its \'Mission\'.',
 ];
 
+const completeContent = [
+
+];
+
 var posts = document.getElementById("posts");
 for(var i=0; i < heading.length; i++){
 
 posts.innerHTML += '<article class="post">\
 <header>\
     <div class="title">\
-        <h2><a href="single.html">'+ heading[i] + '</a></h2>\
+        <h2><a href="images/EBSB_REPORT.pdf">'+ heading[i] + '</a></h2>\
         <p>'+ subHeading[i] +'</p>\
     </div>\
     <div class="meta">\
@@ -40,11 +46,11 @@ posts.innerHTML += '<article class="post">\
         <a href="#" class="author"><span class="name">'+ author[i] +'</span><img src="'+ authorImage[i] +'" alt="" /></a>\
     </div>\
 </header>\
-<a href="single.html" class="image featured"><img src="'+ image[i] +'" alt="" /></a>\
+<a href="images/EBSB_REPORT.pdf" class="image featured"><img src="'+ image[i] +'" alt="" /></a>\
 <p>'+ content[i] +'</p>\
 <footer>\
     <ul class="actions">\
-        <li><a href="single.html" class="button large">Continue Reading</a></li>\
+        <li><a href="images/EBSB_REPORT.pdf" class="button large">Continue Reading</a></li>\
     </ul>\
     <ul class="stats">\
         <li><a href="#">General</a></li>\
@@ -60,11 +66,11 @@ for(var i=0; i < heading.length; i++){
 
 miniPosts.innerHTML += '<article class="mini-post">\
 <header>\
-    <h3><a href="single.html">'+ heading[i] +'</a></h3>\
+    <h3><a href="images/EBSB_REPORT.pdf">'+ heading[i] +'</a></h3>\
     <time class="published">'+ date[i] +'</time>\
     <a href="#" class="author"><img src="'+authorImage[i]+'" alt="" /></a>\
 </header>\
-<a href="single.html" class="image"><img src="'+image[i]+'" alt="" /></a>\
+<a href="images/EBSB_REPORT.pdf" class="image"><img src="'+image[i]+'" alt="" /></a>\
 </article>';
 }
 
@@ -74,10 +80,10 @@ for(var i=0; i < heading.length; i++){
 postsNoImage.innerHTML += '<li>\
         <article>\
             <header>\
-                <h3><a href="single.html">'+ heading[i] +'</a></h3>\
+                <h3><a href="images/EBSB_REPORT.pdf">'+ heading[i] +'</a></h3>\
                 <time class="published">'+ date[i] +'</time>\
             </header>\
-            <a href="single.html" class="image"><img src="'+ authorImage[i] +'" alt="" /></a>\
+            <a href="images/EBSB_REPORT.pdf" class="image"><img src="'+ authorImage[i] +'" alt="" /></a>\
         </article>\
     </li>';
 }
