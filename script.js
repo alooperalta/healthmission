926bcef1c825ee5ee707459f243eba6e5c173d2a
HimeshNayak <Himeshnayak015@gmail.com>
2020-10-04 16:23:33 +0530

    Added new post about HEAL

--- script.js
+++ script.js
@@ -2,34 +2,52 @@
 
 const heading = [
     'Ek Bharat Shrestha Bharat Campaign',
+    'Heal App'
 ];
 
 const subHeading = [
-    'First Report of EBSB Club of Guru Gobind Singh Indraprastha Univeersity',
+    'First Report of EBSB Club of Guru Gobind Singh Indraprastha University',
+    'An app developed to boost your immunity'
 ];
 
 const author = [
     'GGSIPU',
+    'Himesh Nayak'
 ];
 
 const authorImage = [
     'images/ipuLogo.png',
+    'images/healLogo.png'
 ];
 
 const date = [
     'February 22, 2020',
+    'October 04, 2020'
 ];
 
 const image = [
     'images/cllgView.jpg',
+    'images/healBanner.png'
 ];
 
 const content = [
     'The first EBSB day was observed on 14th February, 2020. The member students gathered near the central library area of the University and invoked Goddess of wisdom. Thereafter, they came to the Flag hosting area and paid homeage to the Pulwama Martyrs. The programme of the day started by taking pledge about the fundamental duties of citizens followed by recital of its \'Mission\'.',
+    'Heal app is a personal guide to holistic health and happiness. It can help you to focus on your Physical health and mental health and also aid you to improve your immunity.'
 ];
 
 const completeContent = [
-
+    '',
+    'Heal app is a personal guide to holistic health and happiness. It can help you to focus on your Physical health and mental health and also aid you to improve your immunity.\
+    The app targets mainly five areas which are Physical fitness, Mental fitness, Nutrition, Sleep Cycle and Hygiene. The app provides you with a self check so that you can monitor yourself and know about your strengths and weaknesses to further improve your health. We also provide you with useful information that could really turn your life towards betterment.\
+    Designed with user friendly and hassle-free user interface, the app helps you to HEAL yourself.\
+    So what does the HEAL App do?\
+    - Questionnaire that helps you get a self check\
+    - You get to know about the different aspect of you\
+    - There is ample Information about your Physical health, Mental health and guidance to improve it\
+    - Calm your mind with the soothing meditation tracks in the app.\
+    - An amazing feature of Yoga Nidra; one of the best tracks that will make you sleep like a baby if you follow it before going to bed\
+    - Set alarms with your own motivation mantra and stay energetic\
+    - A section is devoted to physical health, where you learn about different yoga asanas'
 ];
 
 var posts = document.getElementById("posts");
